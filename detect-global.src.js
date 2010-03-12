65e5225e243a65f8e070b8f8224d4ce97b7fb416
Juriy Zaytsev <kangax@gmail.com>
2010-03-12 16:56:51 -0500

    Add TODO. Add `_gaq` to a list of Google Analytics variables (thanks Peter Higgins).

--- detect-global.src.js
+++ detect-global.src.js
@@ -120,7 +120,7 @@
       'Scriptaculous':    'Autocompleter Builder Control Draggable Draggables Droppables Effect Sortable SortableObserver Sound Scriptaculous'.split(' '),
       'Firebug':          'loadFirebugConsole console _getFirebugConsoleElement _FirebugConsole _FirebugCommandLine _firebug'.split(' '),
       'Mozilla':          'Components XPCNativeWrapper XPCSafeJSObjectWrapper getInterface netscape GetWeakReference'.split(' '),
-      'GoogleAnalytics':  'gaJsHost gaGlobal _gat pageTracker'.split(' ')
+      'GoogleAnalytics':  'gaJsHost gaGlobal _gat _gaq pageTracker'.split(' ')
     };
     
     initConfigPopup();
